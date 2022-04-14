be398e6c0cc367684e33b4eabc487bb783d07ce9
junjange <wnswkd486@naver.com>
2022-04-15 02:20:05 +0900

    22.04.15Update
    - 카카오 맵 지도 검색

---
+++ app/src/main/java/com/junjange/pmdkey/adapter/KakaoLocalAdapter.kt
@@ -0,0 +1,47 @@
+package com.junjange.pmdkey.adapter
+
+// 리사이클러 뷰 어댑터 클래스
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.junjange.pmdkey.R
+import com.junjange.pmdkey.data.ModelKakaoLocal
+
+class KakaoLocalAdapter(val itemList: ArrayList<ModelKakaoLocal>): RecyclerView.Adapter<KakaoLocalAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KakaoLocalAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.`list_ltem_kakao_local.xml`, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: KakaoLocalAdapter.ViewHolder, position: Int) {
+        holder.name.text = itemList[position].name
+        holder.road.text = itemList[position].road
+        holder.address.text = itemList[position].address
+        // 아이템 클릭 이벤트
+        holder.itemView.setOnClickListener {
+            itemClickListener.onClick(it, position)
+        }
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val name: TextView = itemView.findViewById(R.id.tv_list_name)
+        val road: TextView = itemView.findViewById(R.id.tv_list_road)
+        val address: TextView = itemView.findViewById(R.id.tv_list_address)
+    }
+
+    interface OnItemClickListener {
+        fun onClick(v: View, position: Int)
+    }
+
+    fun setItemClickListener(onItemClickListener: OnItemClickListener) {
+        this.itemClickListener = onItemClickListener
+    }
+
+    private lateinit var itemClickListener : OnItemClickListener
+}
